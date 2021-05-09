0e2cfe57d821a6544e8c3e7ddd4f54f2629958f7
Ytalo <gydaytalo@gmail.com>
2021-05-09 04:26:46 -0300

    modules rebased

---
+++ src/app/first-component/first-component.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'first-component',
+  template: `
+  <p>first-component</p>
+  `,
+})
+
+export class FirstComponent {
+  constructor() {
+
+  }
+}
